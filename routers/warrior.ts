2222c7577a575389ecccd3854c884baefb960b65
Mariusz <mario.m4@interia.pl>
2022-09-20 15:12:20 +0200

    fixed router /warrior to get number stats from form

--- routers/warrior.ts
+++ routers/warrior.ts
@@ -1,4 +1,5 @@
 import {Router} from "express";
+import {WarriorRecord} from "../records/warrior.record";
 
 export const warriorRouter = Router()
 
@@ -7,5 +8,12 @@ warriorRouter
         res.render('warrior/add-form.hbs')
     })
     .post('/', (req,res) => {
+        const warrior = new WarriorRecord({
+            ...req.body,
+            str: Number(req.body.str),
+            def: Number(req.body.def),
+            stamina: Number(req.body.stamina),
+            agility: Number(req.body.agility),
+        })
         res.render('warrior/warrior-added.hbs')
     })
