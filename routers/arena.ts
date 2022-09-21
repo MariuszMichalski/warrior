89ba18b3d030dc859f16afe76a55a2c4cae454ac
Mariusz <mario.m4@interia.pl>
2022-09-21 18:03:37 +0200

    Added validations to arena

--- routers/arena.ts
+++ routers/arena.ts
@@ -1,5 +1,6 @@
 import {Router} from "express";
 import {WarriorRecord} from "../records/warrior.record";
+import {ValidationError} from "../utils/errors";
 
 export const arenaRouter = Router()
 
@@ -10,6 +11,23 @@ arenaRouter
             warriors
         })
     })
-    .post('/fight', (req,res) => {
+    .post('/fight', async (req,res) => {
+
+        const {warrior1: warrior1Id, warrior2: warrior2Id} = req.body
+
+        if (warrior1Id === warrior2Id) {
+            throw new ValidationError(`You have to choose two different warriors`)
+        }
+
+        const warrior1 = await WarriorRecord.getOne(warrior1Id)
+        const warrior2 = await WarriorRecord.getOne(warrior2Id)
+
+        if (!warrior1) {
+            throw new ValidationError('Cannot find first warrior')
+        }
+        if (!warrior2) {
+            throw new ValidationError('Cannot find second warrior')
+        }
+
         res.render('arena/fight-form.hbs')
     })
