323c591b311277d8caf5937c6b003854e623ed22
Mariusz <mario.m4@interia.pl>
2022-09-21 16:47:09 +0200

    Added warrior select on arena. Added partials

--- routers/arena.ts
+++ routers/arena.ts
@@ -1,10 +1,14 @@
 import {Router} from "express";
+import {WarriorRecord} from "../records/warrior.record";
 
 export const arenaRouter = Router()
 
 arenaRouter
-    .get('/fight-form', (req,res) => {
-        res.render('arena/fight.hbs')
+    .get('/fight-form', async (req,res) => {
+        const warriors = await WarriorRecord.listAll()
+        res.render('arena/fight.hbs', {
+            warriors
+        })
     })
     .post('/fight', (req,res) => {
         res.render('arena/fight-form.hbs')
